c6d5fb60d7868a572517b2437b5d8530d3380fd4
Richard Acquaye <racquaye89@gmail.com>
2023-04-09 13:40:11 +0100

    Add mobile styling

--- src/components/RecipeThumbnail/RecipeThumbnail.js
+++ src/components/RecipeThumbnail/RecipeThumbnail.js
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
 import "./RecipeThumbnail.scss";
+import IcnBookmark from "../Icons/IcnBookmark";
 
 const RecipeThumbnail = ({ recipe }) => {
   return (
-    <Link to={`/recipe/${recipe.id}`}>
-      <article>
-        <img src={recipe.image} alt={recipe.title} />
-        <p>{recipe.title}</p>
+    <Link to={`/recipe/${recipe.id}`} className="recipe-thumb__link">
+      <article className="recipe-thumb">
+        <img
+          className="recipe-thumb__img"
+          src={recipe.image}
+          alt={recipe.title}
+        />
+        <div className="recipe-thumb__details">
+          <div className="recipe-thumb__txt">
+            <p className="recipe-thumb__title">{recipe.title}</p>
+            <p className="recipe-thumb__author">{recipe.sourceName}</p>
+          </div>
+          <div className="recipe-thumb__actions">
+            <IcnBookmark />
+          </div>
+        </div>
       </article>
     </Link>
   );
