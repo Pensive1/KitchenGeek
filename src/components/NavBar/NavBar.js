854bc00b0b99c9bef40275790c5bf5c9eedd1e91
Richard Acquaye <racquaye89@gmail.com>
2023-04-10 21:35:26 +0100

    Add nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,32 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import IcnExplore from "../Icons/IcnExplore";
+import IcnCookBook from "../Icons/IcnCookBook";
+import "./NavBar.scss";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isActive, setIsActive] = useState(1);
+
   return (
     <>
-      <Link to="/">Discover</Link>
-      <Link to="/cookbook">My Cookbook</Link>
-      <Link to="/shopping">Shopping List</Link>
+      <nav className="navigation">
+        <NavLink
+          className="navigation__link"
+          to="/"
+          onClick={() => setIsActive(1)}
+        >
+          <IcnExplore isActive={isActive === 1 ? true : false} />
+          Discover
+        </NavLink>
+        <NavLink
+          className="navigation__link"
+          to="/cookbook"
+          onClick={() => setIsActive(2)}
+        >
+          <IcnCookBook isActive={isActive === 2 ? true : false} />
+          My Cookbook
+        </NavLink>
+      </nav>
     </>
   );
 };
